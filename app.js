5b5462d317e9bb3fb212c3a87e29526515599508
SheikhAlUsama <82088182+SheikhAlUsama@users.noreply.github.com>
2021-05-19 21:42:51 +0500

    star pattern assignment

---
+++ app.js
@@ -0,0 +1,67 @@
+// for(i=1; i<=7; i++){
+//     for(j=7; j>=i; j--){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+
+
+
+
+
+
+//    a
+
+// for(i=1; i<=4; i++){
+//     for(j=1; j<=5; j++){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+
+
+    // b 
+// for(i=1; i<=5; i++){
+//     for(j=1; j<=i; j++){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+ 
+//   e
+
+// var row = 5;
+// for(i=1; i<=row; i++){
+//     for(k=1; k<=(row-i); k++ ){
+//         document.write("&nbsp")
+//     }
+//     for(j=1; j<=i; j++){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+
+    //  d
+
+// for(i=1; i<=10; i++){
+//     for(j=1; j<=i; j++){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+// for(i=1; i<=10; i++){
+//     for(j=10; j>=i; j--){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+
+//    c
+  
+// for(i=1; i<=5; i++){
+//     for(j=5; j>=i; j--){
+//         document.write("*")
+//     }
+//     document.write("</br>")
+// }
+
